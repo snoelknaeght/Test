37c38edc64b69d79c1657d53902f1fcc306777f2
snoelknaeght <stephane5724@hotmail.fr>
2025-06-13 11:30:08 +0200

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import globals from "globals";
+
+export default [
+    {
+        languageOptions: {
+            ecmaVersion: 2022,
+            sourceType: "module",
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+                myCustomGlobal: "readonly"
+            }
+        }
+        // ...other config
+    }
+];
